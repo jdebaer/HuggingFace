c8ba3ba39e0aa488f3405d94d5b616d0554e4a5f
Joachim De Baer <jdebaer@gmail.com>
2024-04-20 15:01:50 -0600

    Greedy gpt working.

--- TextGen/gpt.py
+++ TextGen/gpt.py
@@ -22,12 +22,18 @@ with torch.no_grad():
         iteration['input'] = tokenizer.decode(input_ids[0])
 
         # Generate the next input id.
+        # Type of output is CausalLMOutputWithCrossAttentions.
+        # CausalLMOutputWithCrossAttentions.logits is (batch_size, sequence_length, config.vocab_size)
         output = model(input_ids = input_ids)
 
         # output is (batch_size, seq_len, embed_size) and we only need the very last (complete, so :) context vector to predict the next id.
-        next_id_logits = ouput.logits[0, -1, :]
+        next_id_logits = output.logits[0, -1, :]
+
+        #print(next_id_logits.size())
+        #torch.Size([50257])
 
         next_id_probs = torch.softmax(next_id_logits, dim=-1)
+        #torch.Size([50257])
 
         # We want to be able to select any id above some probability threshold, so let's sort them. 
         sorted_ids = torch.argsort(next_id_probs, dim=-1, descending=True)
@@ -37,11 +43,22 @@ with torch.no_grad():
             prob = next_id_probs[id].cpu().numpy()
 
             token_choice = (
-                f'{tokenizer.decode(token_id)} ({100 * prob:.2f}%)'	
+                f'{tokenizer.decode(id)} ({100 * prob:.2f}%)'	
             )
-            iteration[f'Choice {choice_idx+1}'] = token_choice
+            iteration[f'Choice {idx+1}'] = token_choice
+        
+        # print(sorted_ids.size())
+        # torch.Size([50257])
+
+        # print(input_ids.size())
+        # torch.Size([1, 4]) where the '4' is the dim that keeps growing.
 
         input_ids = torch.cat([input_ids, sorted_ids[None, 0, None]], dim = -1)
+        # input_ids has 2 dims.
+        # sorted_ids has 1 dim.
+        # sorted_ids[None,0] has 1 dim and only has the first element from sorted_ids.
+        # sorted_ids[None, 0, None] has 2 dims.        
+
         iterations.append(iteration)
 
 print(pd.DataFrame(iterations))
