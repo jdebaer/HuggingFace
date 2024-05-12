2188c54615fe9c285eec944915d7f0cb2a46078a
Joachim De Baer <jdebaer@gmail.com>
2024-05-11 18:04:03 -0600

    FAISS index implementation.

--- NoLabels/FAISS_sim_search.py
+++ NoLabels/FAISS_sim_search.py
@@ -73,6 +73,8 @@ ds_dict = ds_dict.map(prepare_labels, batched=True)
 
 import torch
 from transformers import AutoTokenizer, AutoModel
+from collections import defaultdict
+from datasets import DatasetDict
 
 model_ckpt = 'miguelvictor/python-gpt2-large'
 tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
@@ -80,33 +82,89 @@ model = AutoModel.from_pretrained(model_ckpt)
 
 def mean_pooling(model_output, attention_mask):
     
+    # model_output['0'] is last_hidden_state which is a tensor.
     token_embeddings = model_output[0]
-    
-    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+    # Dim of token_embedddings is torch.Size([16, 128, 1280]) which is (batch_size, seq_len, embed_dim).
 
-    sum_embeddings = torch.sum(token_embeddings * input_mask_expanded, 1)
+    # We now need to turn the padding attention masks into a tensor that we can sum up with the above.
+    # attention_mask is torch.Size([16, 128]).
     
-    sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+    # input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+
+    input_mask_expanded = attention_mask.unsqueeze(-1)
+    # Dim of input_mask_expanded = torch.Size([16, 128, 1]).
+    # Note that the value in the new dimension corresponds to the value that was in corresponding index in the previous dimension, so it's 0 or 1.
+
+    input_mask_expanded = input_mask_expanded.expand(token_embeddings.size()).float()
+    # Dim of input_mask_expanded = torch.Size([16, 128, 1280]).
+    # Note that the existing values are projected out, so it's 1280 0's or 1's.
+    
+    # Sum up each row in dimension 1 (seq_len) so we sum up all the values for embedding[0] and then for [1] -> [1279].
+    sum_embeddings = torch.sum(token_embeddings * input_mask_expanded, 1)
+    # Dim is back to torch.Size([16, 1280]) so one big embedding per sample which is at this point the sum of all embeddings.
+    # To get the mean, we need to divide by the number of elements that were not zero, and we need that in dim (16,1280) as well.
+    
+    # We don't want to div by zero but we also don't want to change the smallest value (which should be 1 if it was all padding).
+    sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)			
+    # Dim is (16,1280).
 
     return sum_embeddings / sum_mask
 
 def embed_text(samples):
 
     encodings = tokenizer(samples['text'], padding=True, truncation=True, max_length=128, return_tensors='pt')
+    # This is <class 'transformers.tokenization_utils_base.BatchEncoding'> which has 2 elements, input_ids and attention_mask.
+
+    for idx in range(len(encodings.input_ids)):
+        if 0 in encodings.attention_mask[idx]:
+            print('We are here')
+            print(encodings.input_ids[idx])
+            print(encodings.attention_mask[idx])
+            exit(0)
+
+    exit(0)
 
     with torch.no_grad():
 
+        # model_output type is BaseModelOutputWithPastAndCrossAttentions which is <class 'collections.OrderedDict'>.
         model_output = model(**encodings)
+        #for key, value in model_output.items(): 
+        #    print(key) 
+        ## last_hidden_state is [0]
+        ## past_key_values is [1]
 
     pooled_embeds = mean_pooling(model_output, encodings['attention_mask'])
+    # Dim is torch.Size([batch_size, embed_size]).
 
     return{'embedding': pooled_embeds.cpu().numpy()}    
 
 tokenizer.pad_token = tokenizer.eos_token
 
-embs_train = ds_dict['train'].map(embed_text, batched=True, batch_size=16)
-embs_valid = ds_dict['valid'].map(embed_text, batched=True, batch_size=16)
-embs_test = ds_dict['test'].map(embed_text, batched=True, batch_size=16)
+
+fracs = [0.25, 0.25, 0.25]
+
+ds_dict_mini = defaultdict(DatasetDict)
+
+for frac in fracs:
+    # shuffle and downsample as per fracs
+    for split in ds_dict:
+        ds_dict_mini[split] = ds_dict[split].shuffle(seed=0).select(range(int(frac * ds_dict[split].num_rows)))  # Shuffle to avoid topic bias when downsampling.
+
+# The line below does not seems to change the padding token id from the default 50256 to something else. Should not matter since we're not including
+# them in the pooling anyway.
+tokenizer.pad_token = tokenizer.eos_token
+
+embs_train = ds_dict_mini['train'].map(embed_text, batched=True, batch_size=16)
+embs_valid = ds_dict_mini['valid'].map(embed_text, batched=True, batch_size=16)
+embs_test = ds_dict_mini['test'].map(embed_text, batched=True, batch_size=16)
+
+# At this point these datasets have a column called 'embedding' which contains for each sample a pooled embedding vector capturing the meaning.
+# We're now adding the FAISS index, which based on such embeddings and which allows to querty efficiently based on vector similarity.
+# After adding this index you can do KNN see p. 277.
+
+embs_train.add_faiss_index('embedding')
+
+
 
 
 
